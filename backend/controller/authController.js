a65d9cef8a863104671dd912e3bc171fa6b94426
Jeeva <jeevarathinam518@gmail.com>
2025-07-22 08:25:40 +0530

    add: Added register and login controller flow

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,8 +1,70 @@
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const register = (request, response) =>{
-    response.send("Successfull Registered");
+
+export const register = async (request, response) =>{
+        
+    try{
+    
+        console.log('BODY:', request.body);
+
+        const {firstName, lastName, email, password} = request.body;
+        
+        if(!(firstName && lastName && email && password)){
+            response.status(400).send("Please enter all the information");
+        }
+
+        const isExistingUser = await User.findOne({email});
+        if(isExistingUser){
+            return response.status(400).send("User Already Exists with the same email!!!");
+        }
+
+        var hashedPassword = await bcrypt.hash(password, 2);
+
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            password : hashedPassword
+        });
+
+        console.log("S " , process.env.SECRET_KEY);
+
+        const token = jwt.sign({id:user._id, email}, process.env.SECRET_KEY, {
+            expiresIn : '1h'
+        });
+
+        user.token = token;
+        user.password = undefined;
+        response.status(200).json({message: "You have successfully Registered!!!", user});
+    
+    } catch (error) {
+        response.status(400).json({message:"Internal server error while registering!!" + error});
+    }
 }
 
-export const login = (request, response) =>{
-    response.send("Successfull Logged In!!");
+export const login = async (request, response) =>{
+   
+    try{
+
+        const {email, password} = request.body;
+        console.log(request.body);
+        const user = await User.findOne({email});
+        
+        if(!user){
+            return response.status(400).send("You don't have a registered User with this email, Please register!!");
+        }
+
+        const token = jwt.sign({id:user._id, email}, process.env.SECRET_KEY, {
+            expiresIn : '1h'
+        });
+
+        user.token = token;
+        user.password = undefined;
+        response.status(200).json({message: "You have successfully LoggedIn!!!", user});
+    }
+    catch(error){
+        response.status(500).json({message:"Internal server error while loggin In!!" + error});
+    }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './routes/router.js';
 import auth from './routes/auth.js';
+import DBConnection from './database/database.js';
 
 dotenv.config();
 
+DBConnection();
+
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 app.use("/auth", auth);
