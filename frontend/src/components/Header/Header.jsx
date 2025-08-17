389144247e3996508d5b7df0c6a4c479c05a7c2e
Jeeva <jeevarathinam518@gmail.com>
2025-08-17 17:00:46 +0530

    add: Added new styles to header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import api from "../../axios";
 import "./Header.css";
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = async () => {
     try {
@@ -18,9 +19,36 @@ const Header = () => {
   return (
     <header className="app-header">
       <div className="nav-left">
-        <Link to="/explore" className="nav-link">Explore</Link>
-        <Link to="/problems" className="nav-link">Problems</Link>
-        <Link to="/contest" className="nav-link">Contest</Link>
+         <Link 
+          to="/" 
+          className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+        >
+          Home
+        </Link>
+        <Link 
+          to="/explore" 
+          className={`nav-link ${location.pathname === "/explore" ? "active" : ""}`}
+        >
+          Explore
+        </Link>
+        <Link 
+          to="/problems" 
+          className={`nav-link ${location.pathname === "/problems" ? "active" : ""}`}
+        >
+          Problems
+        </Link>
+        <Link 
+          to="/contest" 
+          className={`nav-link ${location.pathname === "/contest" ? "active" : ""}`}
+        >
+          Contest
+        </Link>
+         <Link 
+          to="/addProblems" 
+          className={`nav-link ${location.pathname === "/addProblems" ? "active" : ""}`}
+        >
+          Add Problems
+        </Link>
       </div>
       <div className="nav-right">
         <button onClick={handleLogout} className="logout-btn">Logout</button>
