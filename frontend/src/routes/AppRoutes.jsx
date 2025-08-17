330c2da2d3abf0cac874a3591fc52338ea052e65
Jeeva <jeevarathinam518@gmail.com>
2025-08-17 14:01:49 +0530

    add: Added frontend routes files

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../components/Login/Login';
+import Register from '../components/Register/Register';
+import ProblemList from '../components/ProblemList/ProblemList';
+import ProblemPage from '../components/ProblemPage/Problempage';
+import ContestPage from '../components/Contest/Contest';
+import ExplorePage from '../components/Explore/ExplorePage';
+
+const AppRoutes = () => {
+return (
+    <Routes>
+      {/* <Route path="/" element={<Dashboard />} /> */}
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/problems" element={<ProblemList />} />
+      <Route path="/problems/:problemId" element={<ProblemPage />} />
+      <Route path='/contest'  element={<ContestPage/>}/>
+      <Route path='/explore'  element={<ExplorePage/>}/>
+
+      {/* <Route path='/contestPage'  element={<ContestPage/>}/> */}
+
+      {/* <Route path="/problems/:id" element={<ProblemDetail />} /> */}
+    </Routes>
+    );
+};
+
+export default AppRoutes;
